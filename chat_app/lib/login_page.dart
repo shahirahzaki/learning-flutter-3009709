69cf5c06bb7855535e2068803eb16f1c610826d5
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2024-11-01 01:16:26 +0800

    update to latest

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,65 +1,99 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  LoginPage({Key? key}) : super(key: key);
 
   void loginUser() {
-    print('login successful!');
+    log('username: ${usernameController.text}', name: 'usernameController');
+    log('password: ${passwordController.text}', name: 'usernameController');
+    log('login successful!');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.5),
-              ),
-              Text(
-                'Welcome back! \n You\'ve been missed!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                    color: Colors.blueGrey),
-              ),
-              Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200,
-              ),
-
-              //TODO: Add Username & Password text fields
-
-              ElevatedButton(
-                  onPressed: loginUser,
-                  child: Text(
-                    'Click me!',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
-                  )),
-              GestureDetector(
-                onTap: () {
-                  //todo: Navigate to browser
-                  print('Link clicked!');
-                },
-                child: Column(
-                  children: [
-                    Text('Find us on'),
-                    Text('https://poojabhaumik.com'),
-                  ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Let\'s sign you in!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.5),
                 ),
-              )
-            ],
+                Text(
+                  'Welcome back! \n You\'ve been missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.blueGrey),
+                ),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+
+                //TODO: Add Username & Password text fields
+
+                TextField(
+                  controller: usernameController,
+                  // onChanged: (value) {
+                  //   //takes value parameter
+                  //   print('value: $value');
+                  // },
+                  decoration: InputDecoration(
+                    hintText: 'Add your username',
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Add your password',
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+                ElevatedButton(
+                    onPressed: loginUser,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    )),
+                GestureDetector(
+                  onTap: () {
+                    //todo: Navigate to browser
+                    print('Link clicked!');
+                  },
+                  child: Column(
+                    children: [
+                      Text('Find us on'),
+                      Text('https://poojabhaumik.com'),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
