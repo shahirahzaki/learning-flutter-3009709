72c52e1fba85898065cc2d47dacf55fd484cb405
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2024-10-31 15:40:54 +0800

    changes according to vid

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -1,2 +1,49 @@
-
 //TODO: Recreate the counter app
+
+// import 'dart:math' as math;
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class CounterStatefulDemo extends StatefulWidget {
+  const CounterStatefulDemo({Key? key}) : super(key: key);
+
+  @override
+  State<CounterStatefulDemo> createState() => _CounterStatefulDemoState();
+  // {
+  //   State<CounterStatefulDemo> stateClassAssociatedWithThisWidget =
+  //       _CounterStatefulDemoState();
+  //   return stateClassAssociatedWithThisWidget;
+  // }
+}
+
+class _CounterStatefulDemoState extends State<CounterStatefulDemo> {
+  int counter = 0;
+
+  void incrementCounter() {
+    // this is called increment method
+    setState(() {
+      counter++;
+      // log('number of count: $counter');
+    });
+    log('Counter: $counter', name: 'CounterStatefulDemo');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementCounter,
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
