1334f1d93ade68b4b445be9854d486482650ff0a
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2024-10-31 22:36:08 +0800

    update to latest change

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,42 +1,90 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // ThemeMode themeMode = ThemeMode.system;
+
+  void loginUser() {
+    log(
+      'Clicked',
+      name: 'ElevatedButton',
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+    final isDarkMode = brightness == Brightness.dark;
+
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                style: TextStyle(
                   fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
+                  color:
+                      // themeMode == ThemeMode.dark ? Colors.black : Colors.white,
+                      isDarkMode ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.5,
+                ),
+                textAlign: TextAlign.center,
+              ),
 
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
+                  color: Colors.blueGrey,
+                ),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
 
-            //TODO: Add Login Button
+              ElevatedButton(
+                onPressed: loginUser,
+                child: Text(
+                  'Click me!',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  log(
+                    'Clicked',
+                    name: 'TextButton',
+                  );
+                },
+                child: Text(
+                  'https://shirazaki.com',
+                ),
+              ),
 
-            //TODO: Add URL Text Button
-          ],
+              //TODO: Add Login Button
+
+              //TODO: Add URL Text Button
+            ],
+          ),
         ),
       ),
     );
--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:chat_app/constants.dart';
 
 void main() {
   runApp(ChatApp());
@@ -10,7 +11,17 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Chat App",
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(
+        colorSchemeSeed: ColorSelection.deepPurple.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: ColorSelection.deepPurple.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      // theme: ThemeData(primarySwatch: Colors.yellow),
       home: LoginPage(),
     );
   }
