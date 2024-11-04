a8dc3dfc936b974910356cfd8784063db80f37e6
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2024-11-04 10:04:11 +0800

    latest

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -4,10 +4,37 @@ import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
-
+  ChatPage({Key? key}) : super(key: key);
 
   //TODO: Create mock list
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+      id: '1',
+      text: 'First text',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(
+        userName: 'poojab26',
+      ),
+    ),
+    ChatMessageEntity(
+      id: '1',
+      text: 'Second text',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(
+        userName: 'poojab26',
+      ),
+      imageUrl:
+          'https://img.freepik.com/free-vector/flat-design-atheism-logo_23-2149240532.jpg?t=st=1730628978~exp=1730632578~hmac=1e96fbbfb1669b7f7a3dd6bcfd68d7eca8673ef9068c55919eb4a69cb6b6853f&w=740',
+    ),
+    ChatMessageEntity(
+      id: '1',
+      text: 'Third text',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(
+        userName: 'poojab26',
+      ),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
@@ -29,19 +56,17 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return ChatBubble(
-                        alignment: index % 2 == 0
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        entity: ChatMessageEntity(
-                            id: '1234',
-                            text: 'Hello this is Pooja!!!!!',
-                            createdAt: DateTime.now().millisecondsSinceEpoch,
-                            author: Author(userName: 'poojab26')));
-                  })),
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return ChatBubble(
+                    alignment: index % 2 == 0
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    entity: _messages[index]);
+              },
+            ),
+          ),
           ChatInput(),
         ],
       ),
